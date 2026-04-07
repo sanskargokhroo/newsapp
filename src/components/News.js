08dfacba19b90de607e34cbfa8b770d5b33ea9e9
sanskargokhroo <sanskargokhroo7@gmail.com>
2026-04-07 12:26:21 +0530

    3

--- src/components/News.js
+++ src/components/News.js
@@ -6,8 +6,6 @@ import InfiniteScroll from "react-infinite-scroll-component";
 
 const News = (props)=> {
     const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState([true]);
-    const [page, setPage] = useState(1);
     const [totalResults, setTotalResults] = useState(0);
 
   
@@ -15,15 +13,22 @@ const News = (props)=> {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
+  // eslint-disable-next-line
   const updateNews = async()=> {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-    setLoading(true);
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    setArticles(parsedData.articles);
-    setTotalResults(parsedData.totalResults);
-    setLoading(false);
+    const url = `https://gnews.io/api/v4/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey}&max=${props.pageSize}`;
+    
+    try {
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      setArticles(parsedData.articles || []);
+      // GNews API free tier doesn't support pagination, so limit totalResults to stop infinite loading
+      setTotalResults((parsedData.articles || []).length); 
+    } catch (error) {
+      console.error(error);
+      setArticles([]);
+    }
+    
     
     props.setProgress(100);
   }
@@ -31,18 +36,13 @@ const News = (props)=> {
   useEffect(() => {
     document.title = `NewsAdda - ${capitalizeFirstLetter(props.category)}`;
     updateNews();
-  }, []);
+    // eslint-disable-next-line
+  }, [props.category]);
 
 
   const fetchMoreData = async () => {
-   
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
-     setPage(page + 1); 
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    setArticles(articles.concat(parsedData.articles));
-    setTotalResults(parsedData.totalResults);
-
+    // Free tier of GNews doesn't support page parameter, 
+    // so this function won't be called as hasMore is now false
   };
 
  
@@ -63,7 +63,7 @@ const News = (props)=> {
               return ( 
               <div className="col-md-4" key={element.url}>
                   <NewsItems title={element.title ? element.title : ""}
-                    description={element.description? element.description.slice(0, 88): ""}imageUrl={element.urlToImage}newsUrl={element.url}author={element.author ? element.author : "Unknown"}date={element.publishedAt? new Date(element.publishedAt).toGMTString(): "Unknown"}
+                    description={element.description? element.description.slice(0, 88): ""}imageUrl={element.image}newsUrl={element.url}author={element.author ? element.author : "Unknown"}date={element.publishedAt? new Date(element.publishedAt).toGMTString(): "Unknown"}
                     source={element.source.name} />
                 </div>)
             })}
